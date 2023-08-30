aea5235421d53aee8c7b1d0aaad9d07de549a993
Tibor Völcker <tiborvoelcker@hotmail.de>
2023-08-30 21:15:37 +0200

    Move training from `main.py` into `train.py`

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-# from gymnasium import make
-# from stable_baselines3.ppo import PPO
+from gymnasium import make
+from stable_baselines3.ppo import PPO
 
-# from kamisado.agents.human import HumanAgent
-# from kamisado.agents.ppo import path
-# from kamisado.wrappers import wrap
+from kamisado.agents.human import HumanAgent
+from kamisado.agents.ppo import path
+from kamisado.wrappers import wrap
 
-# env = wrap(
-#     make("kamisado/Game-v0", render_mode="human"),
-#     tournament_opponent=HumanAgent,
-#     reward_action=False,
-# )
+env = wrap(
+    make("kamisado/Game-v0", render_mode="human"),
+    tournament_opponent=HumanAgent,
+    reward_action=False,
+)
 
-# model = PPO.load(path.parent / path.stem, env=env)
+model = PPO.load(path.parent / path.stem, env=env)
 
-# # watch the trained agent
-# truncated, terminated = False, False
-# obs, info = env.reset()
-# print("Model's color: ", "black" if info["agent_color"] == 0 else "white")
+# watch the trained agent
+truncated, terminated = False, False
+obs, info = env.reset()
+print("Model's color: ", "black" if info["agent_color"] == 0 else "white")
 
-# while not truncated and not terminated:
-#     action, _ = model.predict(obs, deterministic=True)
-#     obs, reward, truncated, terminated, info = env.step(action)
-#     print("Reward: ", reward)
-
-from kamisado.agents.ppo import train
-from kamisado.agents.simple import LookForWinAgent
-
-if __name__ == "__main__":
-    train(
-        2000000,
-        tournament_opponent=LookForWinAgent,
-        reward_action=False,
-    )
+while not truncated and not terminated:
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, truncated, terminated, info = env.step(action)
+    print("Reward: ", reward)
---
+++ train.py
@@ -0,0 +1,13 @@
+"""
+  Copyright (c) Tibor Völcker <tibor.voelcker@hotmail.de>
+  Created on 30.08.2023
+"""
+from kamisado.agents.ppo import train
+from kamisado.agents.simple import LookForWinAgent, RandomAgent
+
+if __name__ == "__main__":
+    train(
+        1000000,
+        tournament_opponent=RandomAgent,
+        reward_action=False,
+    )
